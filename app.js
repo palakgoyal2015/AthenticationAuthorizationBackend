ef055461e3860eea488b3e4e9248b388e91bf424
Palak Goyal <palakgoyal0717@gmail.com>
2025-04-05 22:06:08 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+const express=require("express")
+const app=express()
+const userModel=require('./models/user')
+const cookieParser=require("cookie-parser")
+const bcrypt=require("bcrypt")
+const postModel=require("./models/post")
+const jwt=require("jsonwebtoken")
+app.set("view engine","ejs")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+app.post("/register",async(req,res)=>{
+    const {name,username,age,email,password}=req.body
+   
+    let user=await userModel.findOne({email});
+    if(user) return res.status(500).send("User already resgistered");
+
+    bcrypt.genSalt(10,function(err,salt){
+      bcrypt.hash(password,salt,async (err,hash)=>{
+        let userCreated=await userModel.create({
+            name,
+            username,
+            age,
+            email,
+            password:hash
+        });
+        let token=jwt.sign({email:email,userid:userCreated._id},"shhhh");
+        res.cookie("token",token)
+        console.log(token)
+        res.send("registered")
+       
+      })
+    })
+
+   
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+app.get("/profile",isLoggedIn,async(req,res)=>{
+  let user=await userModel.findOne({email:req.userCreated.email})
+  console.log(user)
+    res.render('profile')
+    
+})
+app.post("/login/create",async(req,res)=>{
+    let {email,password}=req.body
+
+    let user=await userModel.findOne({email:email})
+    if(!user) return res.status(500).send("internal Server Error")
+    bcrypt.compare(password,user.password,function(err,result){
+if(result){
+    let token=jwt.sign({email:email,userid:user._id},"shhhh");
+    res.cookie("token",token)
+    res.status(200).redirect("/profile")
+}else{
+    return res.redirect("/login")
+}
+})
+})
+app.get("/logout",async(req,res)=>{
+    res.cookie("token","");
+    res.render("login")
+})
+function isLoggedIn(req,res,next){
+   if(req.cookies.token==="") return res.redirect("/login")
+    else{
+    let data=jwt.verify(req.cookies.token,"shhhh")
+    req.userCreated=data
+    next()
+    }
+   
+}
+app.listen(3000)
